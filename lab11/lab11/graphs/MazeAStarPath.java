287943469f14d70729037b064e91adc758832faa
qaok <201644606@qq.com>
2023-11-17 15:18:29 +0800

    第一次提交

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,11 +1,15 @@
 package lab11.graphs;
 
+import java.util.PriorityQueue;
+import java.util.Comparator;
 /**
- *  @author Josh Hug
+ *  @author qaok
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
+    private int targetX;
+    private int targetY;
     private boolean targetFound = false;
     private Maze maze;
 
@@ -14,13 +18,35 @@ public class MazeAStarPath extends MazeExplorer {
         maze = m;
         s = maze.xyTo1D(sourceX, sourceY);
         t = maze.xyTo1D(targetX, targetY);
+        this.targetX = targetX;
+        this.targetY = targetY;
         distTo[s] = 0;
         edgeTo[s] = s;
+    }
+    
+    private class Node {
+        private int value;
+        private int priority;
+        
+        public Node(int v) {
+            this.value = v;
+            // 此处的优先级为v到source的距离 + v到target的距离
+            this.priority = distTo[v] + h(v);
+        }
+    }
+    
+    private class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.priority - o2.priority;
+        }
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int curX = maze.toX(v);
+        int curY = maze.toY(v);
+        return Math.abs(curX - targetX) + Math.abs(curY - targetY);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +57,28 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
+        Node node = new Node(s);
+        pq.add(node);
+        marked[s] = true;
+        
+        while (!pq.isEmpty()) {
+            node = pq.poll();
+            for (int w : maze.adj(node.value)) {
+                if (!marked[w]) {
+                    edgeTo[w] = node.value;
+                    distTo[w] = distTo[node.value] + 1;
+                    marked[w] = true;
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                        return;
+                    }
+                    pq.add(new Node(w));
+                }
+            }
+        }
+        
     }
 
     @Override
--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,8 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
 /**
- *  @author Josh Hug
+ *  @author qaok
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +10,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(s);
+        marked[s] = true;
+        
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    announce();
+                    if (w == t) {
+                        targetFound = true;
+                        return;
+                    }
+                    q.enqueue(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
